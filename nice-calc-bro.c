712928de794a6f1d7b4af5b1852455f7bdd511a6
HarunAkbulut <58506555+HarunAkbulut@users.noreply.github.com>
2022-06-26 23:02:54 +0300

    Create nice-calc-bro.c

---
+++ nice-calc-bro.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+//*--- Start of Code ---*//
+
+int main()
+{
+    int a, b;
+    printf("give first number: ");
+    scanf("%d", &a);
+    printf("give second number: ");
+    scanf("%d", &b);
+    printf("%d\n", a+b);
+}
