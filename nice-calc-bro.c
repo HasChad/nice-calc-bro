61220c24437f2a9e699f0991993aa456b4cffdb9
HarunAkbulut <58506555+HarunAkbulut@users.noreply.github.com>
2022-06-27 17:49:13 +0300

    added functions

--- nice-calc-bro.c
+++ nice-calc-bro.c
@@ -2,62 +2,113 @@
 
 //*--- Start of Code ---*//
 
-int main()
+void add(int count)
 {
-    int a, b, calcu = 1;
+    int res = 0, num[count];
+
+    printf("-- addition --\n");
+
+    for (int i = 0; i < count; ++i)
+    {
+        printf("give num: ");
+        scanf("%d", &num[i]);
+        res = num[i] + res;
+    }
+
+    printf("\nresult: %d", res);
+}
+
+void sub(int count)
+{
+    int res = 0, num[count];
+
+    printf("-- subtraction --\n");
+
+    for (int i = 0; i < count; ++i)
+    {
+        printf("give num: ");
+        scanf("%d", &num[i]);
+        res = num[i] - res;
+    }
+
+    printf("\nresult: %d", res);
+}
+
+void mult(int count)
+{
+    int res = 1, num[count];
+
+    printf("-- multiplication --\n");
+
+    for (int i = 0; i < count; ++i)
+    {
+        printf("give num: ");
+        scanf("%d", &num[i]);
+        res = num[i] * res;
+    }
+
+    printf("\nresult: %d", res);
+}
+
+void div(int count)
+{
+    int res = 1, num[count];
+
+    printf("-- division --\n");
+
+    for (int i = 0; i < count; ++i)
+    {
+        printf("give num: ");
+        scanf("%d", &num[i]);
+        res = num[i] / res;
+    }
+
+    printf("\nresult: %d", res);
+}
+
+void main()
+{
+    int a, b, calcu = 1, countOfNum;
     
-    do{
+    while(1 == 1)
+    {
         printf("0. close calc\n1. addition\n2. subtraction\n3. multiplication\n4. division\n");
         printf("what calculation you want to do? ");
         scanf("%d", &calcu);
-        printf("\n");
         
-        if (!(calcu >= 0 && calcu <= 4))
+        if (calcu = 0)
+        {
+            break;
+        }
+
+        printf("how many numbers you gonna use? ");
+        scanf("%d", &countOfNum);
+        printf("\n");
+
+        if (calcu = 0);
+        
+        if (!(calcu >= 1 && calcu <= 4))
             printf("please give valid input\n");
 
         else
             switch (calcu)
             {
-                case 0:
-                calcu = 0;
-                break;
-
                 case 1:
-                printf("-- addition --\n");
-                printf("give first number: ");
-                scanf("%d", &a);
-                printf("give second number: ");
-                scanf("%d", &b);
-                printf("\nresult: %d\n", a+b);
+                add(countOfNum);
                 break;
 
                 case 2:
-                printf("-- subtraction --\n");
-                printf("give first number: ");
-                scanf("%d", &a);
-                printf("give second number: ");
-                scanf("%d", &b);
-                printf("\nresult: %d\n", a-b);
+                sub(countOfNum);
                 break;
 
                 case 3:
-                printf("-- multiplication --\n");
-                printf("give first number: ");
-                scanf("%d", &a);
-                printf("give second number: ");
-                scanf("%d", &b);
-                printf("\nresult: %d\n", a*b);
+                mult(countOfNum);
                 break;
 
                 case 4:
-                printf("-- division --\n");
-                printf("give first number: ");
-                scanf("%d", &a);
-                printf("give second number: ");
-                scanf("%d", &b);
-                printf("\nresult: %d\n", a/b);
+                div(countOfNum);
                 break;
             }
-        printf("|-*--*---*---*---*--*-|\n\n");
-    }while(calcu != 0);
+        printf("\n|-*--*---*---*---*--*-|\n\n");
+    };
 }
