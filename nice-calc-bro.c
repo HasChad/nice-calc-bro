7e531c262ae8e11cb076d6e534fea2f8decceb22
HarunAkbulut <58506555+HarunAkbulut@users.noreply.github.com>
2022-06-27 18:40:11 +0300

    bug fix and optimization

--- nice-calc-bro.c
+++ nice-calc-bro.c
@@ -66,49 +66,47 @@ void div(int count)
     printf("\nresult: %d", res);
 }
 
-void main()
+int main()
 {
-    int a, b, calcu = 1, countOfNum;
+    int calcu, countOfNum;
     
     while(1 == 1)
     {
         printf("0. close calc\n1. addition\n2. subtraction\n3. multiplication\n4. division\n");
         printf("what calculation you want to do? ");
         scanf("%d", &calcu);
+    
+        if (calcu == 0)
+            return 0;
         
-        if (calcu = 0)
-        {
-            break;
-        }
-
-        printf("how many numbers you gonna use? ");
-        scanf("%d", &countOfNum);
-        printf("\n");
-
-        if (calcu = 0);
-        
-        if (!(calcu >= 1 && calcu <= 4))
-            printf("please give valid input\n");
+        else if (!(calcu >= 1 && calcu <= 4))
+            printf("\nplease give valid input");
 
         else
+        {
+            printf("how many numbers you gonna use? ");
+            scanf("%d", &countOfNum);
+            printf("\n");
+            
             switch (calcu)
             {
                 case 1:
                 add(countOfNum);
                 break;
-
+    
                 case 2:
                 sub(countOfNum);
                 break;
-
+    
                 case 3:
                 mult(countOfNum);
                 break;
-
+    
                 case 4:
                 div(countOfNum);
                 break;
             }
+        }
         printf("\n|-*--*---*---*---*--*-|\n\n");
-    };
+    }
 }
